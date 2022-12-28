c4039412cc542247ee84e08d189edc106be8d068
Bilal Sammour <bilalasmor@gmail.com>
2022-12-28 19:06:53 +0300

    renaming user to note item

--- lib/view/screens/dashboard/notes_items.dart
+++ lib/view/screens/dashboard/notes_items.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:structure_demo/models/shared/id_name_model.dart';
-import 'package:structure_demo/view/screens/dashboard/user_item.dart';
+import 'package:structure_demo/view/screens/dashboard/note_item.dart';
 import 'package:structure_demo/view/shared_widgets/list/generic_list.dart';
 
 class NotesItems extends StatelessWidget {
